83dc4200fc642b00c378a94e11fe483e86d39d4b
OwenTowler-Baldwin <towlerowen@gmail.com>
2022-10-11 13:34:18 +1100

    Enemy Health/Death and Code comments
    Enemy now takes damage from the players attack and when its health is 0 it "Dies"

---
+++ Assets/Scripts/Combat/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 5;//max health of enemy
+    int currentHealth;// the enemys current health
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;//sets current health to max on start
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        currentHealth -= Damage;// current health - damage of player
+
+        //play the damaged animation if there is one
+
+        if(currentHealth <= 0)//if health is less then or equal to 0 call die
+        {
+            Die();
+        }
+    }
+
+    void Die()//die function 
+    {
+        Debug.Log("Enemy Died");
+        //death animation
+
+        //dissable the enemy
+    }
+}
--- Assets/Scripts/Combat/PlayerCombat.cs
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -6,20 +6,22 @@ public class PlayerCombat : MonoBehaviour
 {
     //  public Animator animator;
 
-    public Transform attackPoint;
-    public float attackRange = 0.5f;
-    public LayerMask enemyLayers;
+    public Transform attackPoint; // The point from which the wepons range is calculated
+    public float attackRange = 0.5f;// the range the wepon can attack up to
+    public LayerMask enemyLayers;// defines what an enemy is
+
+    public int attackDamage = 1;// the players damage
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetKeyDown(KeyCode.Mouse0))//triggers when left mouse click is clicked
         {
             Attack();
         }
     }
 
-    void Attack()
+    void Attack()// the attack function 
     {
         //play the attack animation, to be fully implemented once animator is ready
         // animator.SetTrigger(); // name of the trigger will go in the brakets
@@ -34,6 +36,8 @@ public class PlayerCombat : MonoBehaviour
         {
             //damage the enemies
             Debug.Log("Hit" + enemy.name);
+
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);//calls the enemy script and allows damage to be done 
         }
     }
 
